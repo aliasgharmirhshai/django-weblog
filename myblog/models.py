35c1ad666cab3466a46774d9728e8e7028689ff6
aliasgharmirhshai <lmyrshahy3@gmail.com>
2022-02-09 20:23:36 +0330

    First commit

---
+++ myblog/models.py
@@ -0,0 +1,20 @@
+from statistics import mode
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+# Create your models here.
+
+class Post(models.Model):
+    STATUS = (
+        ("draft", 'Draft'),
+        ("published", 'Published'),
+    )
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
+    slug = models.SlugField(max_length=250, unique_for_date="publish")
+    title = models.CharField(max_length=250)
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now) 
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10, choices=STATUS, default='draft')
+
