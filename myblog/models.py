05b84438c62a845e6dc28302131bbc7eb6d6ecd6
aliasgharmirhshai <lmyrshahy3@gmail.com>
2022-02-19 16:36:05 +0330

    paginator

--- myblog/models.py
+++ myblog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from django.urls import reverse
 # Create your models here.
 
 class PublishedManager(models.Manager):
@@ -24,6 +25,9 @@ class Post(models.Model):
     objects = models.Manager()
     published = PublishedManager()
 
+    def get_absolute_url(self):
+        return reverse("myblog:post_detail", args=[self.publish.year,self.publish.month,self.publish.day,self.slug])
+    
     def __str__(self):
         return self.title
 
--- myblog/views.py
+++ myblog/views.py
@@ -1,5 +1,25 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 # Create your views here.
 
+def post_list(request):
+    #posts = Post.objects.all().filter(status="published")
+    posts = Post.published.all()
+    #Create Nest Page
+    paginator = Paginator(posts , 2)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        post = paginator.page(paginator.num_pages)
+    ######
+    return render(request, 'blog/post/list.html', {"posts":posts, 'page':page})
 
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post,slug=post,status="published",publish__year=year,publish__month=month,publish__day=day)
+
+    return render(request, 'blog/post/detail.html', {"post":post})
