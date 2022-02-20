b2ff6d8954afb912b9761e4f775580829813b732
aliasgharmirhshai <lmyrshahy3@gmail.com>
2022-02-20 09:18:42 +0330

    Share Post With Email

--- myblog/urls.py
+++ myblog/urls.py
@@ -6,4 +6,5 @@ app_name = 'myblog'
 urlpatterns = [
     path('', views.PostListView.as_view(), name="post_list"),
     path("<slug:post>/",views.post_detail,name="post_detail"),
+    path('<int:post_id>/share/', views.post_share, name="post_share")
 ]
