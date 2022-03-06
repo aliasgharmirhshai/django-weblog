de64d146cc1c5f95f06c7ad9c5a0f097f76c51e1
aliasgharmirhshai <lmyrshahy3@gmail.com>
2022-03-06 09:44:55 -0500

    Site Rss

--- myblog/urls.py
+++ myblog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .feeds import LatestPostsFeed
 #Create app Urls
 
 app_name = 'myblog'
@@ -8,5 +9,6 @@ urlpatterns = [
     path('', views.post_list, name="post_list"),
     path('tag/<slug:tag_slug>', views.post_list, name="post_list"),
     path("<slug:post>/",views.post_detail,name="post_detail"),
-    path('<int:post_id>/share/', views.post_share, name="post_share")
+    path('<int:post_id>/share/', views.post_share, name="post_share"),
+    path('feed/',LatestPostsFeed(),name="post_feed"),
 ]
